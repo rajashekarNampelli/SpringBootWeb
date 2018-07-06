52bceef1f10824bcde21ee36facd9d959d3c1091
rnampell <rajashekar.nampelli@gmail.com>
2018-07-06 15:21:04 -0500

    initial commit

---
+++ src/main/java/com/springboot/SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.springboot")
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+}
